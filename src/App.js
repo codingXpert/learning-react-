4a33c150721a40fb7a724b368b40d3a2b887652c
Vivek <vivekraj78965.vr@gmail.com>
2022-09-11 09:49:06 +0530

    returning a single component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'   //no need to write this after the react version 17.0
 
-function App() {
+import React from "react"
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <h1> Welcome to my page </h1>
     </div>
-  );
+  )
 }
 
-export default App;
+
+//how react is actually looking the above code
+// const App = () => {
+//   return React.createElement(
+//     "div",
+//     {},
+//     React.createElement("h1" , {} , "Welcome to my page")
+//   );
+// };
+
+export default App
